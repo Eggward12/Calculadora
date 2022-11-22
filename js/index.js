0764eb811a18a7ef5b5de95d22660929fc32a5a7
Cristian <97061228+Eggward12@users.noreply.github.com>
2022-11-22 01:35:32 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+// function resta(){
+//     let numero_1 = parseInt($(".numero1").val());
+//     let numero_2 = parseInt($(".numero2").val());
+//     let resul = numero_1-numero_2;
+//     return resul;
+// }
+
+
+
+$("#sum").click(function(){
+    let numero_1 = parseInt($(".numero1").val());
+    let numero_2 = parseInt($(".numero2").val());
+    let resul = numero_1+numero_2;
+    $(".resul").html(resul);
+})
+
+$("#rest").click(function(){
+    let numero_1 = parseInt($(".numero1").val());
+    let numero_2 = parseInt($(".numero2").val());
+    let resul = numero_1-numero_2;
+    $(".resul").html(resul);
+})
+
+$(".mult").click(function(){
+    let numero_1 = parseInt($(".numero1").val());
+    let numero_2 = parseInt($(".numero2").val());
+    let resul = numero_1*numero_2;
+    $(".resul").html(resul);
+})
+
+$(".divi").click(function(){
+    let numero_1 = parseInt($(".numero1").val());
+    let numero_2 = parseInt($(".numero2").val());
+    let resul = numero_1/numero_2;
+    $(".resul").html(resul);
+})
+
+$(".exp").click(function(){
+    let numero_1 = parseInt($(".numero1").val());
+    let numero_2 = parseInt($(".numero2").val());
+    let resul = numero_1**numero_2;
+    $(".resul").html(resul);
+})
